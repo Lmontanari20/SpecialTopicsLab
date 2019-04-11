ecc63027d9920474833ccf5a931f5b9fe52bac7d
Lucas Montanari <lmontanari@csumb.edu>
2019-04-11 09:56:45 -0700

    pushing special topics lab

---
+++ specialTopics/Assets/LightningScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningScript : MonoBehaviour
+{
+    public GameObject ps;
+    public float sec;
+
+
+    void Start()
+    {
+        StartCoroutine(ActivateCoroutine());
+    }
+
+    private IEnumerator ActivateCoroutine()
+    {
+        yield return new WaitForSeconds(sec);
+        Debug.Log("should be setting active rn");
+        ps.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        ps.SetActive(false);
+        StartCoroutine(ActivateCoroutine());
+    }
+
+
+}
---
+++ specialTopics/Assets/Prefabs/MoveCar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    public float torqueSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(v, 0.0f, 0.0f);
+        Vector3 torque = new Vector3(0.0f, h, 0.0f);
+        //rb.AddForce(movement * speed);
+        rb.AddTorque(torque * torqueSpeed);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(-transform.right * speed);
+        }
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
+        {
+            rb.AddForce(-transform.forward * speed);
+        }
+
+
+    }
+}
